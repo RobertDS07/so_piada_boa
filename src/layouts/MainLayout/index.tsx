55fc99a36f9cff3583bf8eab443c2756dee3cb7f
Robert Damaceno <robertdamacenobob@gmail.com>
2022-05-01 19:13:50 -0300

    feat(MainLayout): putting overflow='hidden' because our site is based in centralized div  with scroll inside of that div

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 
 import Background from "./components/Background";
 
 const MainLayout = ({ children }) => {
   return (
-    <Flex w="100vw" minH="100vh" alignItems="center" justifyContent="center">
+    <Flex
+      w="100vw"
+      minH="100vh"
+      alignItems="center"
+      justifyContent="center"
+      overflow="hidden"
+    >
       <Background />
 
       {children}

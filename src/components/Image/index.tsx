ac60a53f9c6b7155e7e260ee27337421c699908d
Robert Damaceno <robertdamacenobob@gmail.com>
2022-05-01 23:39:36 -0300

    refactor(ImageProps): ImagePropss -> ImageProps

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,11 +2,11 @@ import NextImage, { ImageProps as NextImageProps } from "next/image";
 
 import { Box, BoxProps } from "@chakra-ui/react";
 
-type ImagePropss = Required<Pick<NextImageProps, "src" | "alt" | "objectFit">> &
+type ImageProps = Required<Pick<NextImageProps, "src" | "alt" | "objectFit">> &
   Required<Pick<BoxProps, "w" | "h">> &
   Omit<BoxProps, "w" | "h" | "objectFit">;
 
-const Image = ({ src, alt, w, h, objectFit, ...boxProps }: ImagePropss) => {
+const Image = ({ src, alt, w, h, objectFit, ...boxProps }: ImageProps) => {
   return (
     <Box pos="relative" w={w} h={h} {...boxProps}>
       <NextImage src={src} alt={alt} layout="fill" objectFit={objectFit} />
